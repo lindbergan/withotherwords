e970c03e8cd2027daa862cda55a5f39bcc5941ae
Adrian Lindberg <adrlin@student.chalmers.se>
2018-06-09 21:26:34 +0200

    Updated meta

--- client/src/utils/localizer.js
+++ client/src/utils/localizer.js
@@ -2,9 +2,9 @@ import Swedish from '../locales/sv-SE';
 import English from '../locales/en-US';
 
 export function getWord(word, locale) {
-    switch(locale) {
-        case "sv-SE": return Swedish[word];
-        case "en-US": return English[word];
-        default: return English[word];
-    }
+  switch (locale) {
+    case 'sv-SE': return Swedish[word];
+    case 'en-US': return English[word];
+    default: return English[word];
+  }
 }
