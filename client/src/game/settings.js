1c915e901e9005008d2deb4f4bb7576f61c93119
Adrian Lindberg <adrlin@student.chalmers.se>
2019-06-07 13:54:19 +0200

    Refactored settings

--- client/src/game/settings.js
+++ client/src/game/settings.js
@@ -18,89 +18,30 @@ const renderOptions = (from, to, skip) => {
     ));
 };
 
-const Header = ({ locale }) => (
-  <h1 className="header">{getWord("settings", locale)}</h1>
-);
-
-const SelectTeams = ({ locale, nrOfTeams, handleNrTeamsChange }) => (
-  <div>
-    <p>{getWord("selectTeamsText", locale)}</p>
-    <Select
-      value={nrOfTeams}
-      onChange={e => handleNrTeamsChange(e)}
-      classes={{
-        root: "select-center",
-        select: "select-center",
-        selectMenu: "select-center"
-      }}
-    >
-      {renderOptions(2, 7, 1)}
-    </Select>
-  </div>
-);
-
-const SelectRounds = ({ locale, nrOfRounds, handleNrRoundsChange }) => (
-  <div>
-    <p>{getWord("selectRoundsText", locale)}</p>
-    <Select
-      value={nrOfRounds}
-      onChange={e => handleNrRoundsChange(e)}
-      classes={{
-        root: "select-center",
-        select: "select-center",
-        selectMenu: "select-center"
-      }}
-    >
-      {renderOptions(5, 20, 5)}
-      <MenuItem value={999}>{getWord("indefinite", locale)}</MenuItem>
-    </Select>
-  </div>
-);
-
-const SelectTimeLimit = ({ locale, timeLimit, handleTimeLimitChange }) => (
-  <div>
-    <p>{getWord("selectTimeLimit", locale)}</p>
-    <Select
-      value={timeLimit}
-      onChange={e => handleTimeLimitChange(e)}
-      classes={{
-        root: "select-center",
-        select: "select-center",
-        selectMenu: "select-center"
-      }}
-    >
-      {renderOptions(15, 60, 5)}
-    </Select>
-  </div>
-);
-
-const SelectPassesLimit = ({
-  locale,
-  nrOfPassesLimit,
-  handleNrOfPassesLimitChange
+const TextAndSelect = ({
+  text,
+  value,
+  action,
+  options,
+  indefinite,
+  locale
 }) => (
   <div>
-    <p>{getWord("selectNumberOfPasses", locale)}</p>
+    <p className="select-center">{text}</p>
     <Select
-      value={nrOfPassesLimit}
-      onChange={e => handleNrOfPassesLimitChange(e)}
+      value={value}
+      onChange={e => action(e)}
       classes={{
         root: "select-center",
         select: "select-center",
         selectMenu: "select-center"
       }}
     >
-      {renderOptions(0, 10, 1)}
-      <MenuItem value={999}>{getWord("indefinite", locale)}</MenuItem>
+      {renderOptions(...options)}
+      {indefinite ? (
+        <MenuItem value={999}>{getWord("indefinite", locale)}</MenuItem>
+      ) : null}
     </Select>
-  </div>
-);
-
-const StartButton = ({ locale }) => (
-  <div className="start-game-button">
-    <Button component={Link} to="/game" variant="contained" color="primary">
-      {getWord("finishedWithSettings", locale)}
-    </Button>
   </div>
 );
 
@@ -115,28 +56,44 @@ export const Settings = ({
   document.title = getWord("title-settings", locale);
   return (
     <Layout>
-      <Header locale={locale} />
-      <SelectTeams
+      <h1 className="header">{getWord("settings", locale)}</h1>
+      <TextAndSelect
+        text={getWord("selectTeamsText", locale)}
+        value={state.nrOfTeams}
+        action={handleNrTeamsChange}
+        options={[2, 7, 1]}
+        indefinite={false}
         locale={locale}
-        nrOfTeams={state.nrOfTeams}
-        handleNrTeamsChange={handleNrTeamsChange}
       />
-      <SelectRounds
+      <TextAndSelect
+        text={getWord("selectRoundsText", locale)}
+        value={state.nrOfRounds}
+        action={handleNrRoundsChange}
+        options={[5, 20, 5]}
+        indefinite={false}
         locale={locale}
-        nrOfRounds={state.nrOfRounds}
-        handleNrRoundsChange={handleNrRoundsChange}
       />
-      <SelectTimeLimit
+      <TextAndSelect
+        text={getWord("selectTimeLimit", locale)}
+        value={state.timeLimit}
+        action={handleTimeLimitChange}
+        options={[15, 60, 5]}
+        indefinite={false}
         locale={locale}
-        timeLimit={state.timeLimit}
-        handleTimeLimitChange={handleTimeLimitChange}
       />
-      <SelectPassesLimit
+      <TextAndSelect
+        text={getWord("selectNumberOfPasses", locale)}
+        value={state.nrOfPassesLimit}
+        action={handleNrOfPassesLimitChange}
+        options={[0, 10, 1]}
+        indefinite={true}
         locale={locale}
-        nrOfPassesLimit={state.nrOfPassesLimit}
-        handleNrOfPassesLimitChange={handleNrOfPassesLimitChange}
       />
-      <StartButton locale={locale} />
+      <div className="start-game-button">
+        <Button component={Link} to="/game" variant="contained" color="primary">
+          {getWord("finishedWithSettings", locale)}
+        </Button>
+      </div>
     </Layout>
   );
 };
--- client/src/game/welcomescreen.js
+++ client/src/game/welcomescreen.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import { Button } from "@material-ui/core";
 import { getWord } from "../utils/localizer";
 
+import classNames from "classnames";
+
 import { Layout } from "./layout";
 import { sweLocale, engLocale } from "../utils/localizer";
 
@@ -13,15 +15,9 @@ import "../css/global.css";
 const usFlagUrl = "icons/united-states-of-america-flag-round-icon-64.png";
 const sweFlagUrl = "icons/sweden-flag-round-icon-64.png";
 
-function isUsSelected(locale) {
-  return locale === engLocale ? "selected" : "not-selected";
-}
-function isSweSelected(locale) {
-  return locale === sweLocale ? "selected" : "not-selected";
-}
-
 export const WelcomeScreen = ({ locale, changeLanguage }) => {
   document.title = getWord("title-home", locale);
+
   return (
     <Layout>
       <h1 className="title">{getWord("welcomeText", locale)}</h1>
@@ -31,13 +27,19 @@ export const WelcomeScreen = ({ locale, changeLanguage }) => {
         </p>
         <img
           src={sweFlagUrl}
-          className={`swe-flag grid-item image-grid ${isSweSelected(locale)}`}
+          className={classNames("swe-flag", "grid-item", "image-grid", {
+            selected: locale === sweLocale,
+            notSelected: locale !== sweLocale
+          })}
           onClick={() => changeLanguage(sweLocale)}
           alt="swedish flag"
         />
         <img
           src={usFlagUrl}
-          className={`us-flag grid-item image-grid ${isUsSelected(locale)}`}
+          className={classNames("us-flag grid-item image-grid", {
+            selected: locale === engLocale,
+            notSelected: locale !== engLocale
+          })}
           onClick={() => changeLanguage(engLocale)}
           alt="american flag"
         />
